ebf667d773ff5a746a4134403d37fcd47c51cfb4
Gabriel Cari de Oliveira <gabriel.oliveira@tmaior.com.br>
2024-08-29 16:37:55 -0300

    test push

--- github_action_setup.py
+++ github_action_setup.py
@@ -9,30 +9,31 @@ def run_command(command):
     return result.stdout.strip()
 
 def setup_github_branch_protection_rules(gh_repo_name):
-    branches = ['main']
-    # branches = ['dev', 'stage', 'main']
+    branches = ['dev', 'stage', 'main']
+    bpr_config_file="/tmp/config-bpr"
     for branch in branches:
+        config_for_bpr = f"""echo '{{ \
+                "enforce_admins": null, \
+                "allow_deletions": false, \
+                "allow_force_pushes": false, \
+                "lock_branch": false, \
+                "restrictions": null, \
+                "required_status_checks": {{ \
+                    "strict": true, \
+                    "contexts": ["check_pr_origin_branch"] \
+                }}, \
+                "required_pull_request_reviews": {{ \
+                    "dismiss_stale_reviews": true, \
+                    "required_approving_review_count": 1 \
+                }} \
+            }}' > {bpr_config_file} && \
+            chmod +x {bpr_config_file}
+        """
         bpr = f"""gh api -X PUT /repos/tmaior/{gh_repo_name}/branches/{branch}/protection \
             -H "Accept: application/vnd.github.v3+json" \
-            --field required_pull_request_reviews=null \
-            --field required_status_checks=null \
-            --field enforce_admins=null \
-            --field required_status_checks.strict=true \
-            --field enforce_admins=false \
-            --field required_pull_request_reviews.dismiss_stale_reviews=true \
-            --field required_pull_request_reviews.required_approving_review_count=1 \
-            --field restrictions=null \
-            --field required_status_checks.contexts='[]' \
-            --field allow_force_pushes=false \
-            --field allow_deletions=false
+            --input {bpr_config_file}
             """
-        bpr = f"""gh api -X PUT /repos/tmaior/{gh_repo_name}/branches/{branch}/protection \
-            -H "Accept: application/vnd.github.v3+json" \
-            --field required_status_checks.strict=true \
-            --field required_status_checks.contexts='[]' \
-            --field enforce_admins=null \
-            
-            """
+        run_command(config_for_bpr)
         run_command(bpr)
 
 repo_name = "protection-rule-test"
@@ -40,6 +41,8 @@ setup_github_branch_protection_rules(repo_name)
 
 
 
+            # --field required_status_checks.[contexts[]]='["check_branch"]' \
+# --field "required_status_checks[contexts][]=check_branch" \
 # gh api -X PUT repos/tmaior/protection-rule-test/branches/main/protection \
 #             -H "Accept: application/vnd.github.v3+json" \
 #             --field required_status_checks.strict=true \
